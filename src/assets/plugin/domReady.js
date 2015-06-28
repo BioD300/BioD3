1306ec9a0fb294a09fb21fdac0843fdaa0bb3cc3
Prabhat Kumar <iammachine@users.noreply.github.com>
2015-06-28 23:29:31 +0530

    initial domReady.js - commit.
    domReady.js, wait for the DOM is ready.
    Useful for pausing execution of top level application logic until the DOM is ready for querying/modification.
    http://requirejs.org/

---
+++ src/assets/plugin/domReady.js
@@ -0,0 +1,9 @@
+/*
+ * RequireJS domReady [v2.0.1]
+ * ===========================
+ * wait for the DOM is ready.
+ * http://github.com/requirejs/domReady
+ * @author    : James Burke [http://jrburke.com/]
+ * @license   : MIT or new BSD license.
+ * @Copyright : (c) 2010-2012, The Dojo Foundation All Rights Reserved.
+ */
