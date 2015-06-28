a8d7661dbe6f06560ab406731d2f2fabc961de67
Prabhat Kumar <iammachine@users.noreply.github.com>
2015-06-28 22:28:57 +0530

    initial queue.js - commit.
    queue.js, is an asynchronous helper library for JavaScript written by Mike Bostock.
    BioD3 have JSON files that should be loaded in parallel.
    https://github.com/mbostock/queue

---
+++ src/assets/plugin/queue.js
@@ -0,0 +1,9 @@
+/*!
+ * Queue.js [v1.0.7]
+ * =================
+ * Yet another asynchronous helper library for JavaScript.
+ * https://github.com/mbostock/queue
+ * @author  : Mike Bostock [http://bost.ocks.org/ | mike@ocks.org]
+ * @date    : Jan 16, 2014
+ * =======================
+ */
