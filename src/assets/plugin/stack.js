fb32a44f0ca86cdd9440789141a69b4c05a07582
Prabhat Kumar <iammachine@users.noreply.github.com>
2015-06-28 22:44:25 +0530

    initial stack.js - commit.
    stack.js, is a presentation library with intuitive, scroll-based navigation.
    https://github.com/mbostock/stack

---
+++ src/assets/plugin/stack.js
@@ -0,0 +1,9 @@
+/*!
+ * Stack.js [v1.0.0]
+ * =================
+ * A presentation library with intuitive, scroll-based navigation.
+ * https://github.com/mbostock/stack
+ * @author  : Mike Bostock [http://bost.ocks.org/ | mike@ocks.org]
+ * @date    : Sep 08, 2014
+ * =======================
+ */
