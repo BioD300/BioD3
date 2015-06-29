22eaa1d2591c9942f05d2ee91bdeb1df22d9b039
Prabhat Kumar <iammachine@users.noreply.github.com>
2015-06-29 09:44:18 +0530

    initial require.min.js - commit.
    require.min.js, a JavaScript file and module loader.
    https://github.com/jrburke/requirejs & http://requirejs.org/

---
+++ src/assets/libs/require.min.js
@@ -0,0 +1,10 @@
+/*!
+ * RequireJS [v2.1.18]
+ * ===================
+ * A JavaScript file and module loader.
+ * http://github.com/jrburke/requirejs
+ * @author    : James Burke [http://jrburke.com/]
+ * @license   : MIT or new BSD license.
+ * @Copyright : (c) 2010-2015, The Dojo Foundation. All Rights Reserved.
+ * @date      : May 29, 2015
+ */
